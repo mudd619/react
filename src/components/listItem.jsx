7e3fbf716aea1dc3eb38f69d3ef54c4e2d2574cf
Muddasir <mohammed_fw10_064@masai.school>
2021-09-06 19:22:32 +0530

    react

---
+++ src/components/listItem.jsx
@@ -0,0 +1,19 @@
+export function ListItem(){
+    // console.log(props)
+    return <div style={{marginLeft : "5%" }}>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>WEindows phone</li>
+        </ul>
+        <h1>MobileManufacturers</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+}
