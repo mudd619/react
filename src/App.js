57ae97d26a70167fc82b4077efd0936095cc4348
Muddasir <mohammed_fw10_064@masai.school>
2021-09-19 11:58:52 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 //import logo from './logo.svg';
 import './App.css';
-import {Drawer} from './components/navBar';
+import {Toolbar} from './components/navBar';
 
 import styled from "styled-components"
 
 const Div = styled.div`
-      display: grid;
-      grid-template-columns: 8% 20%;
-      margin-bottom: 3%;
-`
-
-const Img = styled.img`
-   width:25%;
-   margin-left: 52%;
+  margin-left: 5%;
+  margin-right: 87%;
+  padding:0.5%;
+  padding-top: 0%;
+  background-color: grey;
+  border-radius: 5px;
 `
 
 function App() {
-  const DrawerItem = ({label,logo})=>{
-    return <Div>
-      <Img src={logo}/>
-      <div style={{fontSize:"1.5vw",color:'gray'}}>{label}</div>
-    </Div>
+  const DrawerItem = ({label})=>{
+    return <div style={{color:'white'}}>{label}</div>
+  }
+
+  const SiteName = ({children})=>{
+    return <Div>{children}</Div>
   }
 
   return (
     <div>
-      <Drawer>
-        <DrawerItem label="Inbox" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-UFdqvIf4Yj4nCvkAxgQQSfY6pom6I-6WhA&usqp=CAU"/>
-        <DrawerItem label="Starred" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMefGQqbPFkku7GB6_EgjtnLQaH04Y3FkKCA&usqp=CAU"/>
-        <DrawerItem label="Send email" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSfNTVo1xGYj7Zpird70n4P-vHANNXuB19-A&usqp=CAU"/>
-        <DrawerItem label="Drafts" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUpIUu-1iWSq4PVCz7wbQmaBCcC5nZgA-Z9A&usqp=CAU"/>
-      </Drawer>
+      <Toolbar>
+        <SiteName>
+          <DrawerItem label="Site name"/>
+        </SiteName>
+        <DrawerItem label="About us"/>
+        <DrawerItem label="Prices"/>
+        <DrawerItem label="Other"/>
+      </Toolbar>
     </div>
   );
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,17 @@
-//import styled from "styled-components"
+import styled from "styled-components"
 
+const Di = styled.div`
+margin-top: 5%;
+   display: grid;
+   grid-template-columns: 70% 12% 10% 10%;
+   border: 1px solid black;
+   padding: 1%;
+   margin-left: 2%;
+   margin-right: 2%;
+   background-color: #7c6666;
+   border-radius: 5px;
+`
 
-export const Drawer = ({children})=>{
-    return <div>{children}</div>
+export const Toolbar = ({children})=>{
+    return <Di>{children}</Di>
 }
