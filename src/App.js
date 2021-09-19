be873d10ff5d10e5b5ecd82b427e6c0b5bd0c3f5
Muddasir <mohammed_fw10_064@masai.school>
2021-09-19 10:51:42 +0530

    side bar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Drawer} from './components/navBar';
+
+import styled from "styled-components"
+
+const Div = styled.div`
+      display: grid;
+      grid-template-columns: 8% 20%;
+      margin-bottom: 3%;
+`
+
+const Img = styled.img`
+   width:25%;
+   margin-left: 52%;
+`
 
 function App() {
+  const DrawerItem = ({label,logo})=>{
+    return <Div>
+      <Img src={logo}/>
+      <div style={{fontSize:"1.5vw",color:'gray'}}>{label}</div>
+    </Div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Drawer>
+        <DrawerItem label="Inbox" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-UFdqvIf4Yj4nCvkAxgQQSfY6pom6I-6WhA&usqp=CAU"/>
+        <DrawerItem label="Starred" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMefGQqbPFkku7GB6_EgjtnLQaH04Y3FkKCA&usqp=CAU"/>
+        <DrawerItem label="Send email" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSfNTVo1xGYj7Zpird70n4P-vHANNXuB19-A&usqp=CAU"/>
+        <DrawerItem label="Drafts" logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUpIUu-1iWSq4PVCz7wbQmaBCcC5nZgA-Z9A&usqp=CAU"/>
+      </Drawer>
     </div>
   );
 }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,6 @@
+//import styled from "styled-components"
+
+
+export const Drawer = ({children})=>{
+    return <div>{children}</div>
+}
